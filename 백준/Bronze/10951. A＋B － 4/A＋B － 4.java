5f4b32e3f55c869063563817f0f66d9898bf18ab
Mistle0108 <mistle0108@gmail.com>
2026-01-20 11:44:09 +0900

    [Bronze V] Title: A+B - 4, Time: 184 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in); 
+
+        int a = 0;
+        int b = 0;
+        
+        while(sc.hasNext()) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a + b);
+		}
+	}	
+}
