01ebb9806c09a47bdbeacdfcb3acc9038ecf2c3b
Mistle0108 <mistle0108@gmail.com>
2026-01-20 10:07:01 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18488 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+					
+		if(minute < 45) {
+			hour--;
+			if(hour < 0 ) hour = 23;
+			
+			minute += 15;
+		}else {
+			minute -= 45;
+		}
+		
+		
+		System.out.println(hour + " " +  minute);
+	}	
+}
