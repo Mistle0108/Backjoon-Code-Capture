14e010d2894c16851e3c04c3190bb9d2f3d7ae2b
Mistle0108 <mistle0108@gmail.com>
2026-01-20 11:14:07 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 808 ms, Memory: 240832 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String caseCount = br.readLine();
+
+        int[] result = new int[Integer.parseInt(caseCount)];   // 결과 저장용
+        int idx = 0;
+
+        for(int i = 0; i < Integer.parseInt(caseCount); i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            result[idx++] = a + b;  // 계산 결과만 저장
+        }
+
+        for(int i = 0; i < Integer.parseInt(caseCount); i++){
+            bw.write(String.valueOf(result[i]));
+            bw.newLine();
+        }
+
+        bw.flush();
+	}	
+}
