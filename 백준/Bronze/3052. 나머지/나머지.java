92e860cdfc9ce20ddfef31f5e1b270dd5c02673b
Mistle0108 <mistle0108@gmail.com>
2026-01-20 21:42:50 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+
+class Main {
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+				
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < 10; i++) {
+			int input = sc.nextInt();
+			set.add(input%42);
+		}
+		
+		System.out.println(set.size());
+	}	
+}
