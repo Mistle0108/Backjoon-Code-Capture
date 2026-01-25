39a2c204d4d2293d81dcd6edb5b3dcd69778d17b
Mistle0108 <mistle0108@gmail.com>
2026-01-25 21:48:52 +0900

    [Bronze I] Title: 진법 변환 2, Time: 136 ms, Memory: 16584 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+        String [] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+
+        int quotient = n / b;
+        int remainder = n % b;     
+        int count = 1;
+       
+        char value = remainder >= 10 ? (char)(remainder + 55) :(char)(remainder + 48);
+
+        String result = "";
+        result = value + result;
+        
+	    while(quotient != 0) {
+	    	remainder = quotient % b;
+	    	quotient  = quotient / b;
+	    	
+	    	count++;
+	    	
+	    	value = remainder >= 10 ? (char)(remainder + 55) :(char)(remainder + 48);
+	    	result = value + result;
+	        
+	    }
+	    
+	    bw.write(result);
+	    
+        bw.flush();                   
+        
+        br.close();
+        bw.close();
+	}	
+}
