4bc3948141470a1eae0f1fe73fd93009347b7b89
Mistle0108 <mistle0108@gmail.com>
2026-02-14 09:08:30 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        
+        bw.write(input*(input-1) + "");
+
+        
+        bw.flush();
+        
+        br.close();
+        bw.close();
+	}
+}
