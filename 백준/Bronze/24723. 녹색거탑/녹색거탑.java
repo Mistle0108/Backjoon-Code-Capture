84e45d7e0386f9093b2504c698825212bb428248
Mistle0108 <mistle0108@gmail.com>
2026-02-14 09:37:03 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        
+        int k = 1;
+        for (int i = 0; i < input; i++) {
+			k *= 2;
+		}
+
+        bw.write(k +"");
+        
+        bw.flush();
+        
+        br.close();
+        bw.close();
+	}
+}
