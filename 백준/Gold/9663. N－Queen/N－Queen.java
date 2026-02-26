d8b8c830555170d53aaef79479835fa2e13c46ef
Mistle0108 <mistle0108@gmail.com>
2026-02-27 00:22:24 +0900

    [Gold IV] Title: N-Queen, Time: 5608 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -25,29 +25,29 @@ class Main {
         br.close();     
 	}
 	
-    static void nQueen(int col) {
-    	if(col == input) {
+    static void nQueen(int row) {
+    	if(row == input) {
     		count++;
     		return;
     	}
     	
     	// 각 row당 col 만큼 반복
-    	for (int row = 0; row < input; row++) {
-    		qArr[col] = row;
-    		if(canPlace(col)) {
-    			nQueen(col + 1);
+    	for (int col = 0; col < input; col++) {
+    		qArr[row] = col;
+    		if(canPlace(row)) {
+    			nQueen(row + 1);
     		}
 		}
     }
 	
     // 같은 열도 아니고 대각선 상으로 겹치지 않을 때 True
-    static boolean canPlace(int col) {
-    	for (int i = 0; i < col; i++) {
+    static boolean canPlace(int row) {
+    	for (int i = 0; i < row; i++) {
     		
-			if((qArr[i] == qArr[col])) {
+			if((qArr[i] == qArr[row])) {
 				return false;
 			} 
-			if(Math.abs(i - col) == Math.abs(qArr[col] - qArr[i])){
+			if(Math.abs(i - row) == Math.abs(qArr[row] - qArr[i])){
 				return false;
 			}
 		}
