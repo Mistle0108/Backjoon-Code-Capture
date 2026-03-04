68a0dcc85f451260c91971711e2e9a863cc024e4
Mistle0108 <mistle0108@gmail.com>
2026-03-05 04:31:51 +0900

    [Gold V] Title: 전깃줄, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    	int n = Integer.parseInt(br.readLine());
+
+    	int[][] arr = new int[n][n];
+    	    	
+    	for (int i = 0; i < n; i++) {
+    		String[] temp = br.readLine().split(" ");
+    	
+    		arr[i][0] = Integer.parseInt(temp[0]);
+    		arr[i][1] = Integer.parseInt(temp[1]);
+		}
+    	
+    	Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+
+    	
+    	int[] dp = new int[n];
+    	Arrays.fill(dp, 1);
+    	
+    	int max = 1;
+    	
+    	for (int i = 1; i < n; i++) {
+    		for (int j = 0; j < i; j++) {
+				if(arr[i][1] > arr[j][1]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+    		max = Math.max(max, dp[i]);
+		}
+
+    	
+    	bw.write(n - max + "");
+    	
+        bw.flush();
+        bw.close();
+        br.close();
+	}
+}
